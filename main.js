ce6f64fa363c07278f8a9a743534d5b9f7d6d8d0
whitson-buck <whitsonbuck@gmail.com>
2024-06-19 14:00:40 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+import OSM from 'homepage/node_modules/ol/source/OSM'
+import TileLayer from 'homepage/node_modules/ol/layer/Tile'
+import {Map, View} from 'homepage/node_modules/ol'
+import {fromLonLat} from 'ol/proj'
+
+new Map({
+    target: 'map-container',
+    layers: [
+        new TileLayer ({
+            source: new OSM(),
+        }),
+    ],
+    view: new View({
+        center: fromLonLat([0,0]),
+        zoom: 2,
+    }),
+});
